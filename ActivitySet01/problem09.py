4ddb3a19afda95d83e6ac576681f9649c5412eb0
Nishit Singh <96938506+Nsk98800@users.noreply.github.com>
2022-06-08 09:59:20 +0000

    done

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,17 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fname=input('enter the file name ')
+fh=open(fname)
+lst=list()
+
+for line in fh:
+  world=line.rstrip().split()
+  for element in world:
+    if lement in line:
+      continue
+    else:
+      lst.append(element)
+
+l=lst.sort()
+print(l)
